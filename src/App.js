fac7f735f885ee68d21bd932691a0ada4001d48f
Erin Davis <51665717+erin-davis@users.noreply.github.com>
2020-06-14 18:27:02 -0500

    changes before reverting

--- src/App.js
+++ src/App.js
@@ -6,51 +6,68 @@ import SkillsPage from "./components/SkillsPage.js"
 import Projects from "./components/Projects.js";
 import About from "./components/About.js";
 import ContactForm from "./components/ContactForm.js";
-import NavBar from "./components/NavBar.js";
+//import NavBar from "./components/NavBar.js";
 
-class App extends React.Component {
-  constructor(){
-    super();
-    console.log('in constructor');
-  }
+function App() {
 
-  componentDidMount(){
-    window.onscroll = function(){scrolling()};
-    console.log('in cdm');
-    let navibar = document.getElementById('navibar');
-    console.log("nav bar", navibar);
-    let sticky = navibar.offsetTop;
-    //adding the sticky class on scroll
-    function scrolling(){
-      if(window.pageYOffset >= sticky){
-        navibar.classList.add('sticky');
-      } else {
-        navibar.classList.remove('sticky');
-      }
-    }
-  }
+  let navbar = document.getElementById('navbar');
+  console.log(navbar);
 
-  render(){
-    console.log('in render');
   return (
     <Router>
       <main>
         <div className="App">
-          <header>
-          <NavBar />
-          </header>
+          <nav className="navigation" id="navbar">
+            <h1 className="site-header">Erin Davis Front-End Web Developer</h1>
+            <div className="nav-links">
+            <Link to="/" className="links">Home</Link>
+            <Link to="/about" className="links">About Me</Link>
+            <Link to="/github" className="links" >Github</Link>
+            <Link to="/linkedin" className="links">LinkedIn</Link>
+            <Link to="/skills" className="links">Skills</Link>
+            <Link to="/projects" className="links">Projects</Link>
+            <Link to="/contactme" className="links">Contact Me</Link>
+            </div>
+          </nav>
           <div className="app-body">
           <LandingPage />
-          <About />
-          <SkillsPage />
           <Projects />
-          <ContactForm />      
+          <SkillsPage />
+          <About />
+          <ContactForm />
+        {/*  <Route exact path="/">
+            <LandingPage />
+          </Route>
+          //commented out <SkillsPage />
+          <Route path="projects">
+            <Projects />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contactme">
+            <ContactForm />
+          </Route>
+
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/skills">
+            <SkillsPage />
+          </Route>
+          <Route path="">
+          </Route>
+          <Route path="">
+          </Route>
+          <Route path="">
+          </Route>
+          <Route path="">
+          </Route>*/}
           </div>
         </div>
       </main>
     </Router>
   );
-}
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { Route, Link} from "react-router-dom";
+import { Route, Link, Switch, NavLink} from "react-router-dom";
 
 export default function NavBar(){
+
   return(
-    <nav className="navigation" id="navibar">
-      <h1 className="site-header">Erin Davis Front-End Web Developer</h1>
-      <div className="nav-links">
-      <Link to="/" className="links">Home</Link>
-      <Link to="/about" className="links">About Me</Link>
-      <Link to="/skills" className="links">Skills</Link>
-      <Link to="/projects" className="links">Projects</Link>
-      <Link to="/github" className="links" >Github</Link>
-      <Link to="/linkedin" className="links">LinkedIn</Link>
-      <Link to="/contactme" className="links">Contact Me</Link>
-      </div>
-    </nav>
+    <header className="navigation">
+      <nav>
+        <NavLink to="/about">About Me</NavLink>
+        <Link to="/home">Home</Link>
+        <Link to="/github">Github</Link>
+        <Link to="linkedin">LinkedIn</Link>
+        <Link to="skills">Skills</Link>
+        <Link to="projects">Projects</Link>
+        <Link to="contactme">Contact Me</Link>
+      </nav>
+    </header>
   )
 }
