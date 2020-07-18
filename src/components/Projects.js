fcb471f4e1d6774b4d3ed608fdf2720f1b7f992d
Erin Davis <51665717+erin-davis@users.noreply.github.com>
2020-07-17 21:56:52 -0500

    added placeholder files and worked on carousel styling for portfolio projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,23 +2,57 @@
 an image, a short 2 sentence description, and a link to repo and the deployed site (heroku probs)*/
 import React from "react";
 import {portProj} from "../data/PortfolioInfo.js";
+import {forward, back} from "../data/tabler-icons";
 
 export default function Projects(props) {
   //console.log("from Projects.js", portProj)
+
+  //slideshow controls
+  // let slideIndex = 1;
+  //
+  //
+  // const plusSlides = n =>{
+  //   showSlides(slideIndex += n);
+  // }
+  //
+  // const currentSlide = n =>{
+  //   showSlides(slideIndex = n);
+  // }
+  //
+  // const showSlides = n =>{
+  //   let i;
+  //   let slide = document.getElementsByClassName('slide');
+  //   let dot = document.getElementsByClassName('dot');
+  //   n > slide.length ? slideIndex = 1 : console.log("n is not larger than slide.length");
+  //   n < 1 ? slideIndex = slide.length : console.log("n is not smaller than 1");
+  //   for (let i = 0; i < slide.length; i++) {
+  //     slide[i].classList.add('disNone');
+  //   }
+  //   for (let i = 0; i < dot.length; i++) {
+  //     dot[i].className = dot[i].classList.toggle('active');
+  //   }
+  //   slide[slideIndex - 1].classList.add = 'block';
+  //   dot[slideIndex - 1].classList.add = ' active';
+  // }
+
+  //end slideshow controls
   return(
-    <div className="projects-page comp">
+    <div className="projects-page comp slideshow-cont">
     <h1>Portfolio Projects</h1>
-    {portProj.map(proj =>{
+    {portProj.map((proj, index) =>{
       return (
-        <div className="project">
+        <div className="project slide fade">
         <h3>{proj.name}</h3>
-        <img src={proj.screenshot} alt={`${proj.name} site screenshot`} className="single-project"/>
-        <p>{proj.summary}</p>
+        <img src={proj.screenshot} alt={`${proj.name} site screenshot`} className="project-screenshot"/>
+        <p className="proj-sum">{proj.summary}</p>
         <a href={proj.repo} target="_blank">Repo</a>
         <a href={proj.site} target="_blank">Site</a>
+        <span className="dot" onClick={`currentSlide(${index})`}></span>
         </div>
       )
     })}
+    <span className="back">{back}</span>
+    <span className="next">{forward}</span>
     </div>
   )
 }
--- src/data/PortfolioInfo.js
+++ src/data/PortfolioInfo.js
@@ -1,43 +1,50 @@
 //this is an object of the portfolio projects that i'll be drawing from
+import proj1 from "../images/projectimg1.jpg";
+import proj2 from "../images/projectimg2.jpg";
+import proj3 from "../images/projectimg3.jpg";
+import proj4 from "../images/projectimg4.jpg";
+import proj5 from "../images/projectimg5.jpg";
+import proj6 from "../images/projectimg6.jpg";
+
 export const portProj = [
   {
     name: "Interactive Music Player",
-    screenshot: "img link here",
+    screenshot: proj1,
     repo: "https://github.com/erin-davis/Music-Player-React",
     site: "link the deployed site here",
     summary: "Here’s the summary of the app and what it does. This is pretty neat!"
   },
   {
     name: "Projects Todo App",
-    screenshot: "img link here",
+    screenshot: proj2,
     repo: "https://github.com/erin-davis/Projects-Todo",
     site: "link the deployed site here",
     summary: "This app holds a list of all the projects I want to do and can add new projects as well."
   },
   {
     name: "Interactive Ecommerce Site",
-    screenshot: "img link here",
+    screenshot: proj3,
     repo: "https://github.com/erin-davis/Ecommerce-Site",
     site: "link the deployed site here",
     summary: "This is an Ecommerce site that updates information in real-time."
   },
   {
     name: "Book Finder App",
-    screenshot: "img link here",
+    screenshot: proj4,
     repo: "https://github.com/erin-davis/Book-Finder",
     site: "link the deployed site here",
     summary: "Pulls data from Google Books API and allows searching and list creation."
   },
   {
     name: "Company Directory",
-    screenshot: "img link here",
+    screenshot: proj5,
     repo: "https://github.com/erin-davis/Company-Directory",
     site: "link the deployed site here",
     summary: "Pulls data from random user generator API and displays their information."
   },
   {
     name: "Personal Sky",
-    screenshot: "img link here",
+    screenshot: proj6,
     repo: "https://github.com/erin-davis/Your-Personal-Sky",
     site: "link the deployed site here",
     summary: "Using the NASA API, the user can view pictures from specific dates"
