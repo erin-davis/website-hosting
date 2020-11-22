30e27d32b5f2e91cfefb399b9bae327779c03131
Erin Davis <51665717+erin-davis@users.noreply.github.com>
2020-11-22 10:08:02 -0600

    first commit

---
+++ precache-manifest.65d78d5b459dc6a9ddbc1c801975087c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ef894fdb29ef9d0b3baa0d5c48a44a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7588114e3816b6c246a3",
+    "url": "/static/css/main.8fe39f3d.chunk.css"
+  },
+  {
+    "revision": "8472d15b08169dcbb39f",
+    "url": "/static/js/2.9f227a37.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.9f227a37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7588114e3816b6c246a3",
+    "url": "/static/js/main.af0ffbb2.chunk.js"
+  },
+  {
+    "revision": "c21fe37de23a3f68f6e0",
+    "url": "/static/js/runtime-main.a68b0f98.js"
+  }
+]);
