b3386c833ebb64d9af18cf0b006d5ba94baaf224
Erin Davis <51665717+erin-davis@users.noreply.github.com>
2020-11-23 23:05:31 -0600

    Updates

---
+++ precache-manifest.f47b66e845f70123fa5193a7c7460f29.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "855c99c6edb353046772b0ece1a1a19f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ad556b249c271d8ff12",
+    "url": "/static/css/main.da7b85de.chunk.css"
+  },
+  {
+    "revision": "6118075e14a87db0bff1",
+    "url": "/static/js/2.b32f5a1f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b32f5a1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad556b249c271d8ff12",
+    "url": "/static/js/main.1d1d0ccd.chunk.js"
+  },
+  {
+    "revision": "c21fe37de23a3f68f6e0",
+    "url": "/static/js/runtime-main.a68b0f98.js"
+  },
+  {
+    "revision": "4a383efa5c7cda4934f83db77a604386",
+    "url": "/static/media/chara-gen-sc12.4a383efa.JPG"
+  },
+  {
+    "revision": "afe4e29ecd9788aa835c92292f4f5300",
+    "url": "/static/media/covid-tracker.afe4e29e.JPG"
+  },
+  {
+    "revision": "babf39c43035d43c1fff7c637d0560fe",
+    "url": "/static/media/evernote-clone.babf39c4.JPG"
+  },
+  {
+    "revision": "3b0fc20e90af003c85872f3dfc3fd068",
+    "url": "/static/media/netflix-clone1.3b0fc20e.JPG"
+  }
+]);
